b1896beb1a5ae17f9844e5903fed626f7611e4db
nathaniel-prog <66939923+nathaniel-prog@users.noreply.github.com>
2021-02-21 15:25:17 +0200

    insert urls

--- Nathaniel/urls.py
+++ Nathaniel/urls.py
@@ -4,6 +4,8 @@ from . views import ChauffeurListView , InvidChauffeurView , HomeView
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import login as auth_login , authenticate
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -16,7 +18,9 @@ urlpatterns = [
    path('sms',views.envoi_sms , name='sendsms'),
    path('radio', views.radio_label , name='radio'),
     path('hello', HomeView.as_view(),name='hello'),
-    path('home2' ,views.home_2 , name='home2')
+    path('home2' ,views.home_2 , name='home2'),
+    path('testing', views.testing , name='testing'),
+    path('multiple', views.envoi_multiple, name='multiple')
 
     #path('pick<int:pk>', pickchauffeur(), name='pick')
 
