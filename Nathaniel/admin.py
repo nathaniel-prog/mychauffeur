af81671502c5283ab4bbad2c1a98af040f7ceab1
nathaniel-prog <66939923+nathaniel-prog@users.noreply.github.com>
2021-07-21 12:25:00 +0300

    Delete Nathaniel directory
    delete nathaniel repository

--- Nathaniel/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from Nathaniel.models import Chauffeur , Score , Post
-
-# Register your models here.
-admin.site.register(Chauffeur)
-admin.site.register(Score)
-admin.site.register(Post)
--- Nathaniel/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NathanielConfig(AppConfig):
-    name = 'Nathaniel'
